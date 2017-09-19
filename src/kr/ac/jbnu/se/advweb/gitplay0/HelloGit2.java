21787d0aabb780200c33cbfceac17941ff634b6a
BongOmoon <ansqhddh@gmail.com>
2017-09-19 10:35:49 +0900

    add message "Hello github"

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
@@ -4,7 +4,7 @@ public class HelloGit2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		
+		System.out.println("Hello github!!");
 		// how are you doing??
 	}
 }
