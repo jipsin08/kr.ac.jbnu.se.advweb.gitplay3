428ef7a23f91328ce9230f769365c88302827ed3
houn524 <houn524@jbnu.ac.kr>
2017-09-19 10:45:10 +0900

    confliction test

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit3.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit3.java
@@ -4,7 +4,8 @@ public class HelloGit3 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		//re-test 뀨뀨까까
+		//re-test �⑤�④퉴源�
+		//confliction
 		// how are you doing??
 	}
 }
