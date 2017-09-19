d5dd0605bb48e6cca72c0766b29bb93c49c66600
User <User@PC-00>
2017-09-19 10:19:38 +0900

    Test 승훈

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
@@ -5,7 +5,7 @@ public class HelloGit4 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		
-		//Test Àå½ÂÈÆ ¿¹¾²
+		//Test Yes I can!!
 		// how are you doing??
 	}
 }
